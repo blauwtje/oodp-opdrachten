c94c012a96f85aa995fccf4058b867d9d0555e42
blauwtje <1078121@hr.nl>
2024-09-12 13:40:52 +0200

    opdracht 1 af

---
+++ week2/W02.1.2O01/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        PrintIsLeapYear(2004);
+    }
+    public static bool IsDivisibleBy(int firstInt, int secondInt) => firstInt % secondInt == 0;
+    public static bool IsLeapYear(int Year)
+    {
+        if ((IsDivisibleBy(Year, 4) == true && IsDivisibleBy(Year, 100) == false) || IsDivisibleBy(Year, 400) == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void PrintIsLeapYear(int Year)
+    {
+        if (IsLeapYear(Year) == true)
+        {
+            Console.WriteLine($"{Year} is a leap year");
+        }
+        else
+        {
+            Console.WriteLine($"{Year} is not a leap year");
+        }
+    }
+}
